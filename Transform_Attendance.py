387a4af10a30424611c94ad00c29ef59d92e2348
Thanakrit031 <thanakrit.sir@student.mahidol.edu>
2024-05-17 16:46:47 +0700

    Update .py

--- Transform_Attendance.py
+++ Transform_Attendance.py
@@ -12,11 +12,8 @@ def Transform_Attendance():
 
     df_attendance['numEnrolledStd'] = df_attendance['attendances'].apply(lambda x: x['numEnrolledStd'] if isinstance(x, dict) else None)
     df_attendance['numPresent'] = df_attendance['attendances'].apply(lambda x: x['numPresent'] if isinstance(x, dict) else None)
-    df_attendance['percentPresent'] = df_attendance['attendances'].apply(lambda x: x['percentPresent'] if isinstance(x, dict) else None)
     df_attendance['numAbsent'] = df_attendance['attendances'].apply(lambda x: x['numAbsent'] if isinstance(x, dict) else None)
-    df_attendance['percentAbsent'] = df_attendance['attendances'].apply(lambda x: x['percentAbsent'] if isinstance(x, dict) else None)
     df_attendance['numLeave'] = df_attendance['attendances'].apply(lambda x: x['numLeave'] if isinstance(x, dict) else None)
-    df_attendance['percentLeave'] = df_attendance['attendances'].apply(lambda x: x['percentLeave'] if isinstance(x, dict) else None)
     df_attendance['classDate'] = df_attendance['attendances'].apply(lambda x: x['classDate'] if isinstance(x, dict) else None)
     df_attendance['classStart'] = df_attendance['attendances'].apply(lambda x: x['classStart'] if isinstance(x, dict) else None)
     df_attendance['classEnd'] = df_attendance['attendances'].apply(lambda x: x['classEnd'] if isinstance(x, dict) else None)
@@ -25,8 +22,4 @@ def Transform_Attendance():
     df_attendance.drop_duplicates()
 
     return df_attendance
-
-if __name__ == "__main__":
-    df = Transform_Attendance()
-    df.to_csv("Attendance.csv")
     
--- Transform_Booking.py
+++ Transform_Booking.py
@@ -4,11 +4,8 @@ from Fetch_Booking import Booking_fetch_data
 
 def Transform_Booking():
     df_booking = Booking_fetch_data()
-    df_booking = df_booking[df_booking['activity.activityName'] == "การเรียนการสอน"]
     df_booking[['semester', 'subjectCode', 'section']] = df_booking['remarks'].str.split('|', expand=True)
     df_booking.drop(columns=['remarks'], inplace=True)
+    
     return df_booking
 
-if __name__ == "__main__":
-    df = Transform_Booking()
-    df.to_csv("Booking.csv")
