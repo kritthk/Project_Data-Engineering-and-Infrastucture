9557e9a7775eb49c5c1e1ce9c6bde7eaecbd4e95
millornine <sundayxo02@gmail.com>
2024-05-18 19:54:18 +0700

    Dags Append to sql

--- dags/Dags.py
+++ dags/Dags.py
@@ -13,6 +13,7 @@ from Get_Temp import Get_Temp
 from Transform_Temp import Transform_Temp
 from Transform_Attendance import Transform_Attendance
 from Transform_Booking import Transform_Booking
+from Postgres_Transform_To_SQL import Save_Data_Tran_func
     
 def Get_Booking_Data_Task(**kwargs):
     ti = kwargs['ti']
@@ -39,10 +40,25 @@ def Transform_Booking_Task(**kwargs):
     booking_data = ti.xcom_pull(task_ids='Get_Booking_Data')
     return Transform_Booking(booking_data)
 
-def Transform_Temp_Task(**kwargs):
+def Transform_Temp_Task(**kwargs):      
     ti = kwargs['ti']
     temp_data = ti.xcom_pull(task_ids='Get_Temp_Data')
     return Transform_Temp(temp_data)
+
+def Save_Data_Tran_Temp(**kwargs):
+    ti = kwargs['ti']
+    data = ti.xcom_pull(task_ids='Transform_Temp_Data')
+    return Save_Data_Tran_func(data,"temperature")
+
+def Save_Data_Tran_Booking(**kwargs):
+    ti = kwargs['ti']
+    data = ti.xcom_pull(task_ids='Transform_Booking_Data')
+    return Save_Data_Tran_func(data,"booking")
+
+def Save_Data_Tran_Attendance(**kwargs):
+    ti = kwargs['ti']
+    data = ti.xcom_pull(task_ids='Transform_Attendance_Data')
+    return Save_Data_Tran_func(data,"attendance")
 
 default_args = {
     'owner': 'Krit',
@@ -97,8 +113,108 @@ with DAG('Project',
         python_callable=Transform_Temp_Task,
         provide_context=True
     )
+
+    Save_Data_Transform_Temp = PythonOperator(
+        task_id='Save_Data_Transform_Temp',
+        python_callable=Save_Data_Tran_Temp,
+        provide_context=True
+    )
+
+    Save_Data_Transform_Booking = PythonOperator(
+        task_id='Save_Data_Transform_Booking',
+        python_callable=Save_Data_Tran_Booking,
+        provide_context=True
+    )
+
+    Save_Data_Transform_Attendance = PythonOperator(
+        task_id='Save_Data_Transform_Attendance',
+        python_callable=Save_Data_Tran_Attendance,
+        provide_context=True
+    )
     
 
-    Get_Token >> Get_Booking_Data >> Get_Attendance_Data >> Transform_Attendance_Data
-    Get_Temp_Data >> Transform_Temp_Data
-    Get_Booking_Data >> Transform_Booking_Data
+    Get_Token >> Get_Booking_Data >> Get_Attendance_Data >> Transform_Attendance_Data >> Save_Data_Transform_Attendance
+    Get_Temp_Data >> Transform_Temp_Data >> Save_Data_Transform_Temp
+    Get_Booking_Data >> Transform_Booking_Data >> Save_Data_Transform_Booking
+
+default_args = {
+    'owner': 'Bank',
+    'depends_on_past': False,
+    'start_date': datetime(2023, 4, 20),
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+with DAG('Project_daily_Update',
+         default_args=default_args,
+         description='A DAG for posting and fetching data Daily Update Data',
+         schedule_interval='@daily',
+         
+         catchup=False) as dag:
+
+    Get_Token = PythonOperator(
+        task_id='Get_Api_Token',
+        python_callable=Get_Api_Token,
+        provide_context=True
+    )
+
+    Get_Booking_Data = PythonOperator(
+        task_id='Get_Booking_Data',
+        python_callable=Get_Booking_Data_Task,
+        provide_context=True
+    )
+
+    Get_Attendance_Data = PythonOperator(
+        task_id='Get_Attendance_Data',
+        python_callable=Get_Attendance_Data_Task,
+        provide_context=True
+    )
+    
+    Transform_Attendance_Data = PythonOperator(
+        task_id='Transform_Attendance_Data',
+        python_callable=Transform_Attendance_Task,
+        provide_context=True
+    )
+    
+    Transform_Booking_Data = PythonOperator(
+        task_id='Transform_Booking_Data',
+        python_callable=Transform_Booking_Task,
+        provide_context=True
+    )
+    
+    Get_Temp_Data = PythonOperator(
+        task_id='Get_Temp_Data',
+        python_callable=Get_Temp,
+        provide_context=True
+    )
+    
+    Transform_Temp_Data = PythonOperator(
+        task_id='Transform_Temp_Data',
+        python_callable=Transform_Temp_Task,
+        provide_context=True
+    )
+
+    Save_Data_Transform_Temp = PythonOperator(
+        task_id='Save_Data_Transform_Temp',
+        python_callable=Save_Data_Tran_Temp,
+        provide_context=True
+    )
+
+    Save_Data_Transform_Booking = PythonOperator(
+        task_id='Save_Data_Transform_Booking',
+        python_callable=Save_Data_Tran_Booking,
+        provide_context=True
+    )
+
+    Save_Data_Transform_Attendance = PythonOperator(
+        task_id='Save_Data_Transform_Attendance',
+        python_callable=Save_Data_Tran_Attendance,
+        provide_context=True
+    )
+    
+
+    Get_Token >> Get_Booking_Data >> Get_Attendance_Data >> Transform_Attendance_Data >> Save_Data_Transform_Attendance
+    Get_Temp_Data >> Transform_Temp_Data >> Save_Data_Transform_Temp
+    Get_Booking_Data >> Transform_Booking_Data >> Save_Data_Transform_Booking
